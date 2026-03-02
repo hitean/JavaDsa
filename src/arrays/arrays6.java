76d211ba089ffc5e07294dc52bf95659cec84bc0
hitean <hiteank@gmail.com>
2026-03-02 22:50:23 +0530

    arrays que 6

--- src/arrays/arrays6.java
+++ src/arrays/arrays6.java
@@ -1,4 +1,16 @@
 package arrays;
-
+//Count even and odd numbers in array.
 public class arrays6 {
+    public static void main(String[] args) {
+        int []nums={61,6549,619,564,5419,519,51,544,54,57,87,21};
+        int even=0;
+        int odd=0;
+        for(int i=0;i<nums.length;i++){
+            if (nums[i]%2==0)  even++;
+            else odd++;
+
+        }
+        System.out.println("No of even elements are: "+even);
+        System.out.println("No of even elements odd: "+odd);
+    }
 }
